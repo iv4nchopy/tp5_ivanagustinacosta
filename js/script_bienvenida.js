195316d1b1f2c26bfbd618f9f1a3eee522a66386
iv4nchopy <githubtareas@gmail.com>
2025-11-21 00:02:41 -0300

    Update script_bienvenida.js

--- js/script_bienvenida.js
+++ js/script_bienvenida.js
@@ -3,7 +3,13 @@ function irAInscripcion() {
 }
 
 function irAExamen() {
-    window.location.href = "templates/examen.html";
+    const nombre = localStorage.getItem("nombreUsuario");
+    if (!nombre) {
+        alert("Debes registrarte primero.");
+        window.location.href = "templates/inscripcion.html";
+    } else {
+        window.location.href = "templates/examen.html";
+    }
 }
 
 document.addEventListener("DOMContentLoaded", () => {
