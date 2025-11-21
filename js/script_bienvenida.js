4abb38cb0288a33dd031d22f153856bc5aba927d
iv4nchopy <githubtareas@gmail.com>
2025-11-20 23:42:21 -0300

    Create script_bienvenida.js

---
+++ js/script_bienvenida.js
@@ -0,0 +1,24 @@
+function irAInscripcion() {
+    window.location.href = "templates/inscripcion.html";
+}
+
+function irAExamen() {
+    window.location.href = "templates/examen.html";
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    let nombre = localStorage.getItem("nombreUsuario");
+
+    if (!nombre) {
+        let nombreIngresado = prompt("Ingresa tu nombre y apellido:");
+        if (nombreIngresado) {
+            localStorage.setItem("nombreUsuario", nombreIngresado);
+            nombre = nombreIngresado;
+        }
+    }
+
+    if (nombre) {
+        document.getElementById("mensajeUsuario").innerText =
+            "Bienvenido, " + nombre + "!";
+    }
+});
